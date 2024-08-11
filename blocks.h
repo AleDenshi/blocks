e45e5f4935525bf2eed9d369a8333a85ecb26e52
Denshi <alex@denshi.org>
2024-08-11 12:45:13 +0400

    Fixed typo on LAX

--- blocks.h
+++ blocks.h
@@ -2,11 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{"LAX ", " TZ='America/los_Angeles' date '+%I:%M %p'",		5,		0},
-	{"DAB ", " TZ='America/New_York' date '+%I:%M %p'",		5,		0},
-	{"FCO ", " TZ='Europe/Rome' date '+%I:%M %p'",		5,		0},
-	{"DXB ", " TZ='Asia/Dubai' date '+%I:%M %p'",		5,		0},
-	{"TPE ", " TZ='Asia/Taipei' date '+%I:%M %p'",		5,		0},
+	{"LAX ", "TZ='America/Los_Angeles' date '+%I:%M %p'",		5,		0},
+	{"DAB ", "TZ='America/New_York' date '+%I:%M %p'",		5,		0},
+	{"FCO ", "TZ='Europe/Rome' date '+%I:%M %p'",		5,		0},
+	{"DXB ", "TZ='Asia/Dubai' date '+%I:%M %p'",		5,		0},
+	{"TPE ", "TZ='Asia/Taipei' date '+%I:%M %p'",		5,		0},
 	{"", "date '+%b %d (%a) %I:%M %p'",					5,		0},
 	{"%", "cat /sys/class/power_supply/BAT0/capacity",	5,		0},
 };
